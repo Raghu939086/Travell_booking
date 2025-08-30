30be5a7bc38b48d3b9b1499b24912fef1b8f9fb0
Raghu939086 <gajamraghu991@gmail.com.com>
2025-08-30 13:28:17 +0530

    added deployment settings ad wsgi

--- travel_booking/wsgi.py
+++ travel_booking/wsgi.py
@@ -1,25 +1,13 @@
-"""
-WSGI config for travel_booking project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-import sys
 import os
+import sys
 
-# Add your project directory
-path = '/home/gajamraghu/Travell_booking'
-if path not in sys.path:
-    sys.path.append(path)
+# Project root directory containing manage.py
+project_home = '/home/gajamraghu/Travell_booking'  
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
-# Set Django settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Travell_booking.settings'
-
-activate_this = '/home/gajamraghu/.virtualenvs/travelenv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(_file_=activate_this))
+# Use the exact project module name for settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'travel_booking.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
