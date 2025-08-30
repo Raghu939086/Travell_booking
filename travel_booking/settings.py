211535a7b027520e233f988bf705c8c6f7e2bddc
Raghu939086 <gajamraghu991@gmail.com.com>
2025-08-30 11:29:22 +0530

    added deployment settings ad wsgi

--- travel_booking/settings.py
+++ travel_booking/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o+qfdn5$(v=o9j1om0ufd8nhasom=p#ngp+(^)f5n25d1p5ul*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gajamraghu.pythonanywhere.com']
 
 
 # Application definition
--- travel_booking/wsgi.py
+++ travel_booking/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
+path = '/home/gajamraghu/Travell_booking'  
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Travell_booking.settings'  
+
+activate_this = '/home/gajamraghu/.virtualenvs/travelenv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(_file_=activate_this))
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel_booking.settings')
-
 application = get_wsgi_application()
